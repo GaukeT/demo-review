b7d04fc4e2ac3088f6f6256ef0e1cc0b3446f1f1
gteijema <g.teijema@afterpay.nl>
2019-02-21 17:18:09 +0100

    add logger

--- src/main/java/nl/gauket/demoreview/controller/PersonController.java
+++ src/main/java/nl/gauket/demoreview/controller/PersonController.java
@@ -1,12 +1,15 @@
 package nl.gauket.demoreview.controller;
 
 import nl.gauket.demoreview.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PersonController {
+    private static final Logger logger = LoggerFactory.getLogger("n.g.d.controller.PersonController");
     private static final List<Person> persons;
 
     static {
@@ -20,11 +23,13 @@ public class PersonController {
 
     @GetMapping(path = "/persons")
     public static List<Person> getPersons() {
+        logger.info("return all persons");
         return persons;
     }
 
     @GetMapping(path = "/persons/{name}")
     public static Person getPerson(@PathVariable("name") String name) {
+        logger.info("return a specific person");
         return persons.stream()
                 .filter(person -> name.equalsIgnoreCase(person.getName()))
                 .findAny().orElse(null);
