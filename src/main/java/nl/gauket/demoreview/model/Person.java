8cab55093a5190c07669ad0fc158ccea2eef3b04
gteijema <g.teijema@afterpay.nl>
2019-02-22 15:06:37 +0100

    rm unused methods

--- src/main/java/nl/gauket/demoreview/model/Person.java
+++ src/main/java/nl/gauket/demoreview/model/Person.java
@@ -4,9 +4,6 @@ public class Person {
     private String name;
     private String email;
 
-    public Person() {
-    }
-
     public Person(String name, String email) {
         this.name = name;
         this.email = email;
@@ -16,15 +13,8 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
