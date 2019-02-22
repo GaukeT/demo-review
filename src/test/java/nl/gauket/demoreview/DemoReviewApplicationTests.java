9e16b229be385db060fd7c9bd49d49ca2c61d4d3
gteijema <g.teijema@afterpay.nl>
2019-02-22 14:43:11 +0100

    update version + minor changes JUnit tests

--- src/test/java/nl/gauket/demoreview/DemoReviewApplicationTests.java
+++ src/test/java/nl/gauket/demoreview/DemoReviewApplicationTests.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoReviewApplicationTests {
 
@@ -18,7 +18,7 @@ public class DemoReviewApplicationTests {
 
     @Test
     public void contextLoads() {
-        assertThat(personController).isNotNull();
+        assertNotNull(personController);
     }
 
 }
